f8daa3ab15c1ee79c8a98c55ee60aa0a194f7d3c
craig fay <craigfay1@gmail.com>
2020-02-12 23:01:20 -0600

    Refactor algebraic()

--- src/notation.rs
+++ src/notation.rs
@@ -10,16 +10,15 @@ pub fn algebraic_move(s: &str, state: GameState) -> Option<Move> {
         destination: 1 as usize,
     };
 
-    let ranks = ['1','2','3','4','5','6','7','8'];
-    let files = ['a','b','c','d','e','f','g','h'];
-    let pieces = ['B','N','R','Q','K'];
+    static ranks: [char; 8] = ['1','2','3','4','5','6','7','8'];
+    static files: [char; 8] = ['a','b','c','d','e','f','g','h'];
+    static pieces: [char; 5] = ['B','N','R','Q','K'];
 
     let chars: Vec<char> = s.chars().collect();
 
     match chars.as_slice() {
-
         [file, rank] => {
-            if files.contains(&file) && ranks.contains(&rank) {
+            if algebraic(s).is_some() {
                 return Some(dummy_move);
             }
             None
@@ -38,21 +37,21 @@ pub fn index(square: usize) -> String {
 
 // Get index notation from algebraic
 pub fn algebraic(s: &str) -> Option<usize> {
-    let mut chars = s.chars();
-    let file = chars.next();
-    let rank = chars.next();
-    let end = chars.next();
+    static ranks: [char; 8] = ['1','2','3','4','5','6','7','8'];
+    static files: [char; 8] = ['a','b','c','d','e','f','g','h'];
 
-    if !rank.is_some() { return None }
-    if !file.is_some() { return None }
-    if end.is_some() { return None }
+    let chars: Vec<char> = s.chars().collect();
 
-    if !file.unwrap().is_alphabetic() { return None }
-    if !rank.unwrap().is_numeric() { return None }
-
-    let file = file.unwrap() as usize - 97;
-    let rank = (rank.unwrap().to_digit(10).unwrap() as usize - 1) * 8;
-
-    Some(file + rank)
+    match chars.as_slice() {
+        [file, rank] => {
+            if files.contains(file) && ranks.contains(rank) {
+                let file = *file as usize - 97;
+                let rank = (rank.to_digit(10).unwrap() as usize - 1) * 8;
+                return Some(file + rank)
+            }
+            None
+        },
+        _ => None
+    }
 }
 
