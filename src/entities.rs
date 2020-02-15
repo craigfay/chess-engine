cb625fd51e168bb6e3ce11ded1c38039c6d9a9d8
craig fay <craigfay1@gmail.com>
2020-02-15 01:01:30 -0500

    Introduce GameState::new_game()

--- src/entities.rs
+++ src/entities.rs
@@ -35,6 +35,45 @@ impl GameState {
         }
         board
     }
+    pub fn new_game() -> GameState {
+        GameState::with_placements(vec![
+            Placement::new(Color::White, PieceType::Rook, 0),
+            Placement::new(Color::White, PieceType::Knight, 1),
+            Placement::new(Color::White, PieceType::Bishop, 2),
+            Placement::new(Color::White, PieceType::Queen, 3),
+            Placement::new(Color::White, PieceType::King, 4),
+            Placement::new(Color::White, PieceType::Bishop, 5),
+            Placement::new(Color::White, PieceType::Knight, 6),
+            Placement::new(Color::White, PieceType::Rook, 7),
+    
+            Placement::new(Color::White, PieceType::Pawn, 8),
+            Placement::new(Color::White, PieceType::Pawn, 9),
+            Placement::new(Color::White, PieceType::Pawn, 10),
+            Placement::new(Color::White, PieceType::Pawn, 11),
+            Placement::new(Color::White, PieceType::Pawn, 12),
+            Placement::new(Color::White, PieceType::Pawn, 13),
+            Placement::new(Color::White, PieceType::Pawn, 14),
+            Placement::new(Color::White, PieceType::Pawn, 15),
+    
+            Placement::new(Color::Black, PieceType::Pawn, 48),
+            Placement::new(Color::Black, PieceType::Pawn, 49),
+            Placement::new(Color::Black, PieceType::Pawn, 50),
+            Placement::new(Color::Black, PieceType::Pawn, 51),
+            Placement::new(Color::Black, PieceType::Pawn, 52),
+            Placement::new(Color::Black, PieceType::Pawn, 53),
+            Placement::new(Color::Black, PieceType::Pawn, 54),
+            Placement::new(Color::Black, PieceType::Pawn, 55),
+    
+            Placement::new(Color::Black, PieceType::Rook, 56),
+            Placement::new(Color::Black, PieceType::Knight, 57),
+            Placement::new(Color::Black, PieceType::Bishop, 58),
+            Placement::new(Color::Black, PieceType::Queen, 59),
+            Placement::new(Color::Black, PieceType::King, 60),
+            Placement::new(Color::Black, PieceType::Bishop, 61),
+            Placement::new(Color::Black, PieceType::Knight, 62),
+            Placement::new(Color::Black, PieceType::Rook, 63),
+        ])
+    }
 }
 
 pub struct Placement {
