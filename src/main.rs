e4e2dd583df6a62757025676214e50b0b2f32d9c
craig fay <craigfay1@gmail.com>
2020-02-21 20:26:15 -0600

    White kingside castle obstruction

--- src/main.rs
+++ src/main.rs
@@ -787,6 +787,21 @@ fn black_queenside_castle_aftermath_test() {
     assert!(!aftermath.squares[56].is_some());
 }
 
+fn white_kingside_castle_obstruction_test() {
+    let mut state = GameState::with_placements(vec![
+        Placement::new(White, King, 4),
+        Placement::new(White, Bishop, 5),
+        Placement::new(White, Rook, 7),
+    ]);
+    state.white_can_castle_kingside = true;
+    let m = Move {
+        origin: 4,
+        destination: 6,
+        piece: King,
+    };
+    assert!(!move_is_legal(&m, &state));
+}
+
 fn main() {
     // Time tests
     let timer = Instant::now();
@@ -844,6 +859,7 @@ fn main() {
     white_queenside_castle_aftermath_test();
     black_kingside_castle_aftermath_test();
     black_queenside_castle_aftermath_test();
+    white_kingside_castle_obstruction_test();
 
     let duration = timer.elapsed();
     println!("Tests finished in {:?}", duration);
